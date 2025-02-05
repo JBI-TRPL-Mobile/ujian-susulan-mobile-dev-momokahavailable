69875a0520b7a01782cca6999acfd4b569ab7872
momokahavailble <balaps790@email.com>
2025-02-05 11:19:33 +0700

    perbaikan kesalahan penulisan route

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:template_project/screens/wellcome_screen.dart';
 import 'package:template_project/screens/register_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
-
-class MyApp extends StatelessWidget{
+ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Flutter Navigation',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
       initialRoute: '/',
       routes: {
-        '/': (context) => WellcomeScreen(),
-        '/register':(context) => RegisterScreen(),
+        '/': (context) => WelcomeScreen(),
+        '/Register':(context) => RegisterScreen(),
       },
     );
   }
-}
+ }
