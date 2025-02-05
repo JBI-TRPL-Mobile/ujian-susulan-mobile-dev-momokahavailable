8bd6d3eb65588bd530ba5ed715bc96506ef909d3
momokahavailble <balaps790@email.com>
2025-02-05 10:31:08 +0700

    perubahan terhadap main dan wellcome untuk navigator ke register

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:template_project/screens/wellcome_screen.dart';
+import 'package:template_project/screens/register_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: WelcomeScreen(),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => WellcomeScreen(),
+        '/register':(context) => RegisterScreen(),
+      },
+    );
+  }
 }
--- lib/screens/wellcome_screen.dart
+++ lib/screens/wellcome_screen.dart
@@ -6,6 +6,18 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade900,
+        title: Text(
+          "Update Nilai",
+          style: GoogleFonts.poppins(
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -20,23 +32,22 @@ class WelcomeScreen extends StatelessWidget {
               ),
             ),
             SizedBox(height: 20),
-
             Text(
               "Hallo User!",
               style: GoogleFonts.poppins(
-                fontSize: 20,
+                fontSize: 22,
                 fontWeight: FontWeight.w600,
-                color: Colors.black87,
+                color: Colors.grey.shade600,
               ),
             ),
             SizedBox(height: 10),
             Text(
-              "Pengalaman Pribadi Anda!",
+              " Pengalaman Pribadi Mengenai Anda",
               style: GoogleFonts.poppins(
                 fontSize: 16,
                 color: Colors.grey.shade600,
               ),
-            ),
+              ),
             SizedBox(height: 30),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -46,29 +57,42 @@ class WelcomeScreen extends StatelessWidget {
                 buildIndicator(false),
               ],
             ),
+            SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/Register');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black87,
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              child: Text(
+                'Go to Register',
+                style: GoogleFonts.poppins(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),
+              ),
+            ),
           ],
         ),
       ),
-      appBar: AppBar(
-        backgroundColor: Colors.grey.shade900,
-        title: Text(
-          "Update Nilai",
-          style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w500),
-        ),
-        centerTitle: true,
-      ),
     );
   }
-
-  Widget buildIndicator(bool isActive) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 4),
-      width: isActive ? 12 : 8,
-      height: isActive ? 12 : 8,
-      decoration: BoxDecoration(
-        color: isActive ? Colors.black87 : Colors.grey.shade400,
-        shape: BoxShape.circle,
-      ),
-    );
-  }
+ Widget buildIndicator(bool isActive) {
+  return Container(
+    margin: EdgeInsets.symmetric(horizontal: 4),
+    width: isActive ? 12 : 8,
+    height: isActive ? 12 : 8,
+    decoration: BoxDecoration(
+      color: isActive ? Colors.black87 : Colors.grey.shade400,
+      shape: BoxShape.circle,
+    ),
+  );
+ }
 }
+       
